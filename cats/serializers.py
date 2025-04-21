380202412970aa4c39a92088184ed0675d2ba8ad
DenisBelikov <DenisBelikov1992@yandex.ru>
2025-04-21 17:19:10 +0300

    kittygram

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,7 +1,9 @@
 # cats/serializers.py
+import webcolors
+
 from rest_framework import serializers
 
-from .models import Achievement, Cat, Owner
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
 
 import datetime as dt
 
@@ -15,15 +17,37 @@ class OwnerSerializer(serializers.ModelSerializer):
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
+
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+
+    def to_representation(self, value):
+        return value
+    # При записи код цвета конвертируется в его название
+
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
 
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True)
     age = serializers.SerializerMethodField()
+    # Теперь поле примет только значение, упомянутое в списке CHOICES
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
@@ -32,3 +56,11 @@ class CatSerializer(serializers.ModelSerializer):
 
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
