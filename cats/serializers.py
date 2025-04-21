a1a32b48686c89548a3ad7b95f22f83534746a6c
DenisBelikov <DenisBelikov1992@yandex.ru>
2025-04-21 08:36:30 +0300

    kittygram

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,34 @@
+# cats/serializers.py
 from rest_framework import serializers
 
-from .models import Cat
+from .models import Achievement, Cat, Owner
+
+import datetime as dt
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'name')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    achievements = AchievementSerializer(many=True)
+    age = serializers.SerializerMethodField()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
